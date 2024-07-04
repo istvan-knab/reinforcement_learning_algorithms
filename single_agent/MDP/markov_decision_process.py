5163c0bdb75fedc8cdfab7caa59397ccbf9d4e2a
istvan-knab <istvan.knab@icloud.com>
2024-07-04 13:12:29 +0200

    Add full mdp

--- single_agent/MDP/markov_decision_process.py
+++ single_agent/MDP/markov_decision_process.py
@@ -11,13 +11,17 @@ if __name__ == "__main__":
      done = False
      print(f"The agent starts from state: {state}")
      trajectory = []
+     G_t = 0
+     gamma = 0.9
 
      while not done:
           action = env.action_space.sample()
           next_state, reward, done, info = env.step(action)
           env.render(mode='human')
+          G_t = reward + gamma * G_t
           trajectory.append([state, action, reward, done, next_state])
           state = next_state
+          print(f'Actual return: {G_t}')
           time.sleep(0.2)
      env.close()
 
